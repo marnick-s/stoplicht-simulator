42c741f8916334f58c013fbe17bd524925d99124
marnick-s <contact@marnick.dev>
2025-04-14 15:32:59 +0200

    new configs

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import time
 import pygame
 import yaml
 import cProfile
 import pstats
 import io
+import os
 from lib.messenger import Messenger
-from lib.screen import screen, WIDTH, HEIGHT, update_screen_size
+from lib.screen import screen, WIDTH, update_screen_size
 from lib.simulation import Simulation
 
 # Laden en schalen van de achtergronden
@@ -18,9 +18,15 @@ background_image = pygame.transform.scale(background_image, (WIDTH, new_height))
 overlay_image = pygame.image.load('assets/overlay.webp').convert_alpha()
 overlay_image = pygame.transform.scale(overlay_image, (WIDTH, new_height))
 
-def load_config(filename="simulation.yaml"):
-    with open(filename, "r") as file:
-        return yaml.safe_load(file)
+def load_config(config_dir="config"):
+    config = {}
+    for filename in os.listdir(config_dir):
+        if filename.endswith(".yaml"):
+            filepath = os.path.join(config_dir, filename)
+            with open(filepath, "r") as file:
+                file_config = yaml.safe_load(file)
+                config.update(file_config)
+    return config
 
 def run_simulation():
     config = load_config()
