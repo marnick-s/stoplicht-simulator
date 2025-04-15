02e2836198fc5010d20fc9ec17a93772c62d0273
marnick-s <contact@marnick.dev>
2025-04-15 20:19:20 +0200

    bridge

--- lib/bridge.py
+++ lib/bridge.py
@@ -3,11 +3,17 @@ from lib.screen import screen, scale_to_display
 
 class Bridge():
     def __init__(self):
+        self.position = (1388, 910)
+        width, height = (107, 30)
         bridge_sprite = pygame.image.load('assets/brug-wegdek.webp').convert_alpha()
-        self.bridge_sprite = pygame.transform.scale(bridge_sprite, (30, 30))
+        self.bridge_sprite = pygame.transform.scale(bridge_sprite, scale_to_display(width, height))
+        self.angle = 33
 
     def update(self):
         pass
 
     def draw(self):
-        screen.blit(self.bridge_sprite, (0, 0))
+        x, y = self.position
+        rotated_sprite = pygame.transform.rotate(self.bridge_sprite, self.angle)
+        rect = rotated_sprite.get_rect(center=scale_to_display(x, y))
+        screen.blit(rotated_sprite, rect.topleft)
