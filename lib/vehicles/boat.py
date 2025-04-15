214842d53a01575aed4b96e6f12519414d1a2efb
marnick-s <contact@marnick.dev>
2025-04-15 15:57:26 +0200

    boat lights

--- lib/vehicles/boat.py
+++ lib/vehicles/boat.py
@@ -1,12 +1,10 @@
-import pygame
-from lib.screen import screen, WIDTH, HEIGHT
 from lib.vehicles.vehicle import Vehicle
 
 class Boat(Vehicle):
-    sprite_width = 60
-    sprite_height = 35
+    sprite_width = 36
+    sprite_height = 20
     image_folder = "boats"
-    speed = 2
+    speed = 1
 
     def __init__(self, path):
         super().__init__(path, self.speed, self.sprite_width, self.sprite_height, self.image_folder)
